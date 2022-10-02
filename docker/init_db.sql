f285cb50a21e509f92ecd6cd51608226eb14e3df
roberto-asir <roberto.asir@gmail.com>
2022-10-02 11:46:37 +0200

    Add files via upload

---
+++ docker/init_db.sql
@@ -0,0 +1,62 @@
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+
+CREATE TABLE IF NOT EXISTS public.games_games_counter (
+    id bigint NOT NULL,
+    player integer NOT NULL,
+    pc integer NOT NULL,
+    CONSTRAINT games_games_counter_pc_check CHECK ((pc >= 0)),
+    CONSTRAINT games_games_counter_player_check CHECK ((player >= 0))
+);
+
+
+CREATE SEQUENCE public.games_games_counter_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.games_games_counter_id_seq OWNED BY public.games_games_counter.id;
+
+
+ALTER TABLE ONLY public.games_games_counter ALTER COLUMN id SET DEFAULT nextval('public.games_games_counter_id_seq'::regclass);
+
+
+INSERT INTO public.games_games_counter VALUES (1, 0, 0);
+
+
+
+SELECT pg_catalog.setval('public.games_games_counter_id_seq', 2, true);
+
+
+ALTER TABLE ONLY public.games_games_counter
+    ADD CONSTRAINT games_games_counter_pkey PRIMARY KEY (id);
+
